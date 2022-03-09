7dfedb5a3f8abe06f6a6763c6eaba90f1a622813
Sakina-IssaNoha <sakinnanoha@gmail.com>
2022-03-09 08:16:30 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,34 @@
+fun main(){
+   names()
+    towns(arrayOf("dodoma","harare","jakarta"))
+numbers(arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62))
+detail(arrayOf("Ali","Wanjiku","Mary"))
+}
+
+fun names(){
+    var x = arrayOf("Sakina","Saint","Sam","Serah")
+    println(x.contentToString())
+}
+
+fun towns(names: Array<String>){
+    names.forEach { names ->
+        println(names.capitalize())
+    }
+}
+
+fun numbers(number:Array<Int>){
+    var sum = number[5]+number[2]
+    println(sum)
+
+var num = number.indexOf(158)
+    println(num)
+
+    var sortedNumber =number.sortedArray()
+    println(sortedNumber.contentToString())
+}
+fun detail(names:Array<String>):Array<String>{
+var name= names
+    println(name.contentToString())
+    return names
+
+}
